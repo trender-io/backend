e1bf98cefbce083175dc794eb8756a4a0cebac5c
Bryan Kate <bkate@eecs.harvard.edu>
2013-11-20 15:38:48 -0500

    write directly to DB

--- twitter_keyword_collector.py
+++ twitter_keyword_collector.py
@@ -2,6 +2,7 @@ import json
 import time
 import re
 import os
+import sqlite3
 import threading
 from Queue import Queue
 from guess_language import guess_language
@@ -12,12 +13,12 @@ from twython import Twython, TwythonStreamer
 if speedups.available:
     speedups.enable()
 
-APP_KEY = 'YRL2ooEoygBWmrqva4dQng'
-APP_SECRET = '8cesaQxDxNykcqyra2ZXC2Yb6StBCsZ0lZXv2IJk'
-OAUTH_TOKEN = '402933727-iCFAaiah9XVGZ4iJtYiCX6UAccXD4eZgzT0s8nKe'
-OAUTH_SECRET = 'C54uvKvXDAyru9dQiUGXSEHHxGXAemBsHullBbAuM1Ww8'
+APP_KEY = 'JckhuMYvTreTPO4zC5fXxA'
+APP_SECRET = 'QSAozi8gvr6urv7UwlHeCbAn83cwQ0TuxtfmbFTgV4'
+OAUTH_TOKEN = '198988714-Xb5Ze2toM9UDHtLtQkWSvkVmHKMcAhlU5kJnkG3Y'
+OAUTH_SECRET = 'WY4cg2u4Oc8LRjfZmKYs7WDbNdHXVSSTNZjZdvdyn2ROy'
 
-WORD_REGEX = re.compile(r'^([a-zA-Z\'\"\?\.\!]+)$')
+WORD_REGEX = re.compile(r'^([a-zA-Z\'\"\?\.\!,]+)$')
 HASH_REGEX = re.compile(r'^(\w+)$')
 
 AREAS = {"los_angeles": Polygon([(-118.66333,33.610045), (-118.66333,34.415973), (-117.702026,34.415973), (-117.702026,33.610045)]),
@@ -27,12 +28,6 @@ AREAS = {"los_angeles": Polygon([(-118.66333,33.610045), (-118.66333,34.415973),
          "new_york": Polygon([(-74.057465,40.579542), (-74.057465,40.860564), (-73.766327,40.860564), (-73.766327,40.579542)])}
 
 LOCS = ",".join([",".join([str(p) for p in c.bounds]) for c in AREAS.values()])
-
-
-# horrible global variable
-queue_ = Queue()
-running_ = True
-outdir_ = 'collected'
 
 
 class SimpleCollector(TwythonStreamer):    
@@ -60,13 +55,13 @@ def collect_location(locs):
                 stream.disconnect()
 
 
-def parse_tweets():
+def parse_tweets():    
+    db = sqlite3.connect(dbfile_)
+    cursor = db.cursor()
+    
     wordfiles = {}
     hashfiles = {}
     linkfiles = {}
-
-    if not os.path.exists(outdir_):
-        os.makedirs(outdir_)
     
     for c in AREAS.keys():
         wordfiles[c] = open(os.path.join(outdir_, 'word_' + c + '.txt'), 'a')
@@ -94,31 +89,95 @@ def parse_tweets():
         
         # parse the tweet for unique words and figure out the language
         tweet_text = tweet['text'].encode('utf-8')
-        words = [m.group().lower() for m in (WORD_REGEX.match(t) for t in tweet_text.split()) if m]
-        lang = guess_language(" ".join(words))
-        words = [w.translate(None, '.?!,\'\"') for w in words]
+        cleaned = [m.group() for m in (WORD_REGEX.match(t) for t in tweet_text.split()) if m]
+        lang = guess_language(" ".join(cleaned))
+        words = [w.lower().translate(None, '.?!,\"') for w in cleaned]
         
         if words and lang == 'en':
-            tstamp = time.time()
-            wordfiles[city].write("%s %s\n" % (str(tstamp), " ".join(words)))
+            tstamp = int(time.time())
+            
+            # save to sqlite db and to a set of files because we don't completely 
+            # trust the sqlite db to not be corrupted at some point
+            cursor.execute("INSERT INTO tweets VALUES (?, (SELECT city_id FROM cities WHERE name=?), ?)", \
+                           (tstamp, city, " ".join(cleaned)))
+            
+            for w in words:
+                cursor.execute("INSERT OR IGNORE INTO vals(val) VALUES (?)", (w,))
+                cursor.execute("INSERT INTO word VALUES " \
+                               "(?, (SELECT city_id FROM cities WHERE name=?), (SELECT val_id FROM vals WHERE val=?))", 
+                               (tstamp, city, w))
+            
+            wordfiles[city].write("%d %s\n" % (tstamp, " ".join(words)))
             
             if tweet['entities']['hashtags']:
                 tags = [m.group().lower() for m in (HASH_REGEX.match(h['text']) for h in tweet['entities']['hashtags']) if m]
-                hashfiles[city].write("%s %s\n" % (str(tstamp), " ".join(tags)))
+                
+                for t in tags:
+                    cursor.execute("INSERT OR IGNORE INTO vals(val) VALUES (?)", (t,))
+                    cursor.execute("INSERT INTO hash VALUES " \
+                                   "(?, (SELECT city_id FROM cities WHERE name=?), (SELECT val_id FROM vals WHERE val=?))", 
+                                   (tstamp, city, t))
+                
+                hashfiles[city].write("%d %s\n" % (tstamp, " ".join(tags)))
                 
             if tweet['entities']['urls']:
                 urls = [u['expanded_url'] for u in tweet['entities']['urls']]
-                linkfiles[city].write("%s %s\n" % (str(tstamp), " ".join(urls)))
+                
+                for u in urls:
+                    cursor.execute("INSERT OR IGNORE INTO vals(val) VALUES (?)", (u,))
+                    cursor.execute("INSERT INTO link VALUES " \
+                                   "(?, (SELECT city_id FROM cities WHERE name=?), (SELECT val_id FROM vals WHERE val=?))", 
+                                   (tstamp, city, u))
+                
+                linkfiles[city].write("%d %s\n" % (tstamp, " ".join(urls)))
                
+            db.commit()
+            
             count += 1 
-            if count % 100 == 0:
+            if count % 100 == 0:                
                 for file in wordfiles.values() + hashfiles.values() + linkfiles.values():
                     file.flush()
+    
+    # done collecting, clean up
+    db.commit()
+    db.close()
     
     for file in wordfiles.values() + hashfiles.values() + linkfiles.values():
         file.close()
 
 
+def make_db():
+    db = sqlite3.connect(dbfile_)
+    cursor = db.cursor()
+
+    cursor.execute("CREATE TABLE vals (val_id INTEGER PRIMARY KEY ASC, val TEXT NOT NULL UNIQUE)")
+    cursor.execute("CREATE TABLE cities (city_id INTEGER PRIMARY KEY ASC, name TEXT NOT NULL UNIQUE)")
+    cursor.execute("CREATE TABLE tweets (tstamp INT, city_id INTEGER, tweet TEXT)")
+    cursor.execute("CREATE TABLE word (tstamp INT, city_id INTEGER, val_id INTEGER)")
+    cursor.execute("CREATE TABLE hash (tstamp INT, city_id INTEGER, val_id INTEGER)")
+    cursor.execute("CREATE TABLE link (tstamp INT, city_id INTEGER, val_id INTEGER)")
+    
+    for city in AREAS:
+        cursor.execute("INSERT INTO cities(name) VALUES (?)", (city,))
+        
+    db.commit()    
+    db.close()
+
+
+# horrible global variables. some of these could be params
+queue_ = Queue()
+running_ = True
+outdir_ = 'collected'
+dbfile_ = os.path.join(outdir_, 'twitter.db')
+
+# setup output
+if not os.path.exists(outdir_):
+    os.makedirs(outdir_)
+
+if not os.path.exists(dbfile_):
+    make_db()
+
+# start a thread for collection and parsing
 threads = [threading.Thread(target=parse_tweets),
            threading.Thread(target=collect_location, args=(LOCS,))]
 
